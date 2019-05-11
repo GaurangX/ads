b7788940d06115d9d7fd6f3bc8827b7d6186c264
Gaurang Upasani <gaurangupasani@gmail.com>
2019-05-10 21:11:35 -0700

    Changed directory structure

--- P0/Task1.py
+++ P0/Task1.py
@@ -73,5 +73,3 @@ if __name__ == "__main__" :
   main()
   #import profile
   #profile.run("main()")
-
-
--- P0/Task3.py
+++ P0/Task3.py
@@ -76,7 +76,10 @@ def extract_codes(rx_list):
   for k,v in rx_list.items():
     for i in range(len(v)):
       result = code_patt.search(v[i])
+<<<<<<< HEAD:Task3.py
       mob = result.group(0)[0]
+=======
+>>>>>>> Changed directory structure:P0/Task3.py
       mob = v[i][0]
       if ((mob != '(')):
         codes.append(result.group(0)[0:4])
@@ -116,6 +119,3 @@ if __name__ == "__main__":
   main()
   #import profile
   #profile.run("main()")
-
-
-
